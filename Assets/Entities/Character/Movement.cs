590517091eb0fa6bb2c14b1c346db55bf7a72ae0
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-18 18:19:15 +0200

    Add grass variants

--- Assets/Entities/Character/Movement.cs
+++ Assets/Entities/Character/Movement.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
