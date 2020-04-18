caf0594c13ac2a81f043fb840c8867012332fc3f
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-18 17:02:12 +0200

    Changed sprite renderer

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BillboardSprite.cs
+using UnityEngine;
+using System.Collections;
+
+public class Billboard: MonoBehaviour {
+
+    public Transform cameraTransform;
+    private Transform myTransform;
+    
+    public bool alignNotLook = true;
+
+    // Use this for initialization
+    void Start () {
+        myTransform = this.transform;
+        cameraTransform = Camera.main.transform;
+    }
+	
+    // Update is called once per frame
+    void LateUpdate () {
+        if (alignNotLook)
+            myTransform.forward = cameraTransform.forward;
+        else
+            myTransform.LookAt (cameraTransform, Vector3.up);
+    }
+}
