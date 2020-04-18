f60f51b31a73e4368b8c213d63924bb0b187b1fe
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-18 18:37:16 +0200

    Player movement

--- Assets/Entities/Character/Movement.cs
+++ Assets/Entities/Character/Movement.cs
@@ -2,15 +2,29 @@
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator _animator;
+    private CharacterController _characterController;
+
+    [SerializeField]
+    public float walkSpeed = 1.0f;
+    
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate ()
     {
-        
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(x, 0, z).normalized;
+
+        var movement = direction * walkSpeed;
+
+        _characterController.Move(movement * Time.deltaTime);
+
+        _animator.SetFloat("speed", Mathf.Abs(movement.magnitude));
     }
 }
