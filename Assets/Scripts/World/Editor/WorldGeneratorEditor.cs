634e62375b9096a56a2e422287ca8a26d5fdcbd0
Jadw1 <jadw1_1@o2.pl>
2020-04-18 20:54:11 +0200

    perlin noise

---
+++ Assets/Scripts/World/Editor/WorldGeneratorEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(WorldGenerator))]
+public class WorldGeneratorEditor : Editor {
+    public override void OnInspectorGUI() {
+        WorldGenerator mapGen = (WorldGenerator)target;
+
+        if (DrawDefaultInspector ()) {
+            if (mapGen.autoUpdate) {
+                mapGen.GenerateMap ();
+            }
+        }
+
+        if (GUILayout.Button ("Generate")) {
+            mapGen.GenerateMap ();
+        }
+    }
+}
